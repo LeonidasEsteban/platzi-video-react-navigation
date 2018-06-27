816524c55cee6298079dfa57440b3f87a7e278ef
Leonidas Esteban <leonidasesteban@gmail.com>
2018-06-26 20:42:00 -0500

    [UPDATE] redux integrations

--- src/app-navigator-with-state.js
+++ src/app-navigator-with-state.js
@@ -2,33 +2,18 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import AppNavigator from './app-navigator';
 import {
-  createReduxBoundAddListener,
-  initializeListeners,
+  reduxifyNavigator,
 } from 'react-navigation-redux-helpers';
 
-const addListener = createReduxBoundAddListener('root');
+const ReduxifyApp = reduxifyNavigator(AppNavigator, 'root');
 
-class AppNavigatorWithState extends Component {
-  componentDidMount() {
-    initializeListeners('root', this.props.navigation);
-  }
-  render() {
-    const navigation = {
-      dispatch: this.props.dispatch,
-      state: this.props.navigation,
-      addListener
-    }
-    return (
-      <AppNavigator
-        navigation={navigation}
-      />
-    )
-  }
+class AppNavigatorWithState extends ReduxifyApp {
+
 }
 
 function mapStateToProps(state) {
   return {
-    navigation: state.navigation
+    state: state.navigation
   }
 }
 
